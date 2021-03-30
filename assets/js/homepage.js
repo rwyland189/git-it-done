9a80a1dcc38386cb71c98bb0fe9cb10cd05b027f
Becca Wyland <rwyland@uwalumni.com>
2021-03-30 18:21:48 -0500

    update request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,19 @@
 // Ensure javascript file is linked correctly
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // Format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // Make a request to the url
+    fetch (apiUrl).then(function(response) {
+        // Before we can use the response data in our code, we have to format the response as JSON
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
+
+var response = fetch("https://api.github.com/users/octocat/repos");
+console.log(response);
 
 getUserRepos();
 
